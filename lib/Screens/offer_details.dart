e0e7c17c17d6d20a241104c2c33da66e920d4963
Mac <mac@Macs-MacBook-Pro.local>
2021-01-26 21:46:24 +0200

    --

--- lib/Screens/offer_details.dart
+++ lib/Screens/offer_details.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:visitorsa/utils/app_localizations.dart';
+import 'package:visitorsa/utils/colors.dart';
 
 class OfferDetailsScreen extends StatefulWidget {
   @override
@@ -35,43 +37,170 @@ class _OfferDetailsScreenState extends State<OfferDetailsScreen> {
           body: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Container(
-              height: MediaQuery.of(context).size.height,
-                decoration: BoxDecoration(
+              decoration: BoxDecoration(
                   color: Colors.white,
-                    border: Border.all(),
-                    borderRadius: BorderRadius.all(Radius.circular(10))),
-                child: Column(children: [
-                  ClipRRect(
-                    borderRadius:BorderRadius.all(Radius.circular(10)) ,
-                    child: Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: 200,
-                      child: Image.asset(
-                        "images/test.jpg",
-                        fit: BoxFit.cover,
-                      ),
+                  border: Border.all(),
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              child: SingleChildScrollView(
+                child: Wrap(
+                  children: [
+                    Column(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 200,
+                            child: Image.asset(
+                              "images/test.jpg",
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                  flex: 1,
+                                  child: SvgPicture.asset(
+                                    'images/dollars_money_bag.svg',
+                                    height: 30.0,
+                                    // width: 5.0,
+                                    allowDrawingOutsideViewBox: true,
+                                  )),
+                              Expanded(
+                                flex: 3,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Price",
+                                      style: TextStyle(
+                                          color: Colors.green,
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(
+                                      height: 8,
+                                    ),
+                                    Text(
+                                      "From EG 6005",
+                                      style:
+                                          TextStyle(color: Colors.grey, fontSize: 15),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                flex: 1,
+                                child: SvgPicture.asset(
+                                  'images/dollar.svg',
+                                  height: 30.0,
+                                  // width: 5.0,
+                                  allowDrawingOutsideViewBox: true,
+                                ),
+                              ),
+                              Expanded(
+                                flex: 3,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Fare",
+                                      style: TextStyle(
+                                          color: Colors.green,
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(
+                                      height: 8,
+                                    ),
+                                    Text(
+                                      "non txt",
+                                      style:
+                                          TextStyle(color: Colors.grey, fontSize: 15),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Divider(
+                          color: Colors.grey,
+                          thickness: 1,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Align(
+                            alignment:
+                                AppLocalizations.of(context).locale.languageCode ==
+                                        "en"
+                                    ? Alignment.centerLeft
+                                    : Alignment.centerRight,
+                            child: Text(
+                              "Conditions",
+                              style: TextStyle(
+                                  color: Colors.green,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Align(
+                            alignment:
+                                AppLocalizations.of(context).locale.languageCode ==
+                                        "en"
+                                    ? Alignment.centerLeft
+                                    : Alignment.centerRight,
+                            child: Text(
+                              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+                              style: TextStyle(color: Colors.grey, fontSize: 15),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 60.0,
+                          child: Center(
+                            child: new Text(
+                                AppLocalizations.of(context).translate('book_flight'),
+                                textAlign: TextAlign.center,
+                                style:
+                                TextStyle(color: Colors.white, fontSize: 20)),
+                          ),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8.0),
+                              color: AppColors.lightGreen //# Color(0xFFf5f5f5)
+                          ),
+                        )
+                      ],
                     ),
-                  ),
-                  SizedBox(height: 8,),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(children: [
-                      Expanded(
-                        flex: 1,
-                          child: Icon(Icons.monetization_on,color: Colors.green,)),
-                      Expanded(
-                        flex: 2,
-                        child: Column(children: [
-                          Text("Price",style: TextStyle(color: Colors.green,fontSize: 18,fontWeight: FontWeight.bold),),
-                          SizedBox(height: 8,),
-                          Text("From EG 6005",style: TextStyle(color: Colors.grey,fontSize: 15),)
-                        ],),
-                      )
-
-                    ],),
-                  )
-                ],),
-
+                  ],
+                ),
+              ),
             ),
           ),
         ),
